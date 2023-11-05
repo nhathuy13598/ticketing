96dfe4cc8c5db019261ead3db228311c94a4972a
nhathuy <nhathuy13598@gmail.com>
2023-11-05 10:13:42 +0700

    Authentication

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,25 @@
 import express from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
-app.get("/api/users/currentuser", (req, res) => {
-    res.send("Hi there");
-});
+app.all("*", async () => {
+    throw new NotFoundError();
+})
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!");
