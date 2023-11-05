3d6a379c1e02e67b3a9557a30d22b92e8b7c6f96
nhathuy <nhathuy13598@gmail.com>
2023-11-05 12:51:02 +0700

    Password

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
-import {json} from 'body-parser';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
@@ -21,6 +23,17 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000!!!");
-});
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log("Connected to db");
+    }
+    catch (err) {
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log("Listening on port 3000!!!");
+    });
+}
+
+start();
